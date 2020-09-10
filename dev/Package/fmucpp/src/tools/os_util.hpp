98ce8fabafe03e6af805217ded4cdec0ebba68e2
iammix <k.mixios@gmail.com>
2020-09-10 11:18:58 +0300

    Ch 10/09/2020 11:18 - create simple ID Generator

---
+++ dev/Package/fmucpp/src/tools/os_util.hpp
@@ -0,0 +1,32 @@
+#ifndef FMUCPP_OS_UTIL_HPP
+#define FMUCPP_OS_UTIL_HPP
+
+#include <string>
+
+namespace
+{
+    std::string get_os()
+    {
+#if _WIN32 || _WIN64
+        #    if _WIN64
+    return "win64";
+#    else
+    return "win32";
+#    endif
+#elif __linux__
+        return "linux64";
+#endif
+    }
+
+    std::string get_shared_library_extension()
+    {
+#if _WIN32
+        return ".dll";
+#elif __linux__
+        return ".so";
+#endif
+    }
+} // namespace
+
+
+#endif //FMUCPP_OS_UTIL_HPP
