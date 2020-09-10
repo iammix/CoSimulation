bddc4eb107c11b3235d3048e399c98228fce18cc
iammix <k.mixios@gmail.com>
2020-09-10 16:25:08 +0300

    Ch 10/09/2020 16:25

---
+++ dev/Package/fmucpp/src/fmi2/cs_fmu.cpp
@@ -0,0 +1,37 @@
+#include <fmicpp/fmi2/cs_fmu.hpp>
+
+#include <utility>
+
+using namespace fmicpp;
+using namespace fmicpp::fmi2;
+
+
+cs_fmu::cs_fmu(std::shared_ptr<fmu_resource> resource,
+               std::shared_ptr<const cs_model_description> md): resource_(std::move(resource)),
+               modelDescription_(std::move(md))
+{}
+
+
+std::string cs_fmu::get_model_description_xml() const
+{
+    return resource_->get_model_description_xml();
+}
+
+std::shared_prt<const cs_model_description> cs fmu::get_model_description() const
+{
+    return modelDescription_;
+}
+
+std::unique_ptr<cs_slave> cs_fmu::new_instance(const bool visible, const bool loggingOn)
+{
+    std::shared_ptr<cs_library> lib = nullptr;
+    auto modelIdentifier = modelDescription_->model_identifier;
+    if (lib_ == nullptr) {
+        lib_ = std::make_shared<cs_library>(modelIdentifier, resource_);
+    }
+    lib = lib_;
+
+    auto c = lib->instantiate(modelIdentifier, fmi2CoSimulation, guid(),
+                              resource_->resource_path(), visible, loggingOn);
+    return std::make_unique<cs_slave>(c, resource_, lib, modelDescription_)
+}
