b0126b3533074c95c7fe709dd0b16575841984ef
iammix <k.mixios@gmail.com>
2020-09-11 15:16:52 +0300

    Ch 11/09/2020 15:16

--- dev/Package/fmucpp/src/fmi2/cs_library.cpp
+++ dev/Package/fmucpp/src/fmi2/cs_library.cpp
@@ -9,4 +9,133 @@ cs_library::cs_library(const std::string& modelIdentifier,
 {
     fmi2SetRealInputDerivatives_ = load_function <fmi2SetRealInputDerivativesTYPE*>(handle_,"fmi2SetRealInputDerivatives");
     fmi2GetRealOutputDerivatives_ = load_function<fmi2GetRealOutputDerivativesTYPE*>(handle_,"fmi2GetRealOutputDerivatives");
+
+    fmi2DoStep_ = load_function<fmi2DoStepTYPE*>(handle_, "fmi2DoStep");
+    fmi2CancelStep_ = load_function<fmi2CancelStepTYPE*>(handle_, "fmi2CancelStep");
+
+    fmi2GetStatus_ = load_function<fmi2GetStatusTYPE*>(handle_, "fmi2GetStatusTYPE");
+    fmi2GetRealStatus_ = load_function<fmi2GetRealStatusTYPE*>(handle_, "fmi2GetRealStatusTYPE");
+    fmi2GetIntegerStatus_ = load_function<fmi2GetIntegerStatusTYPE*>(handle_, "fmi2GetIntegerStatusTYPE");
+    fmi2GetBooleanStatus_ = load_function<fmi2GetBooleanStatusTYPE*>(handle_, "fmi2GetBooleanStatusTYPE");
+    fmi2GetStringStatus_ = load_function<fmi2GetStringStatusTYPE*>(handle_, "fmi2GetStringStatusTYPE");
 }
+
+bool cs_library::step(
+        fmi2Component c,
+        const fmi2Real currentCommunicationPoint,
+        const fmi2Real communicationStepSize,
+        const bool noSetFMUStatePriorToCurrentPoint)
+
+{
+    return update_status_and_return_true_if_ok(fmi2DoStep_(c, currentCommunicationPoint, communicationStepSize, noSetFMUStatePriorToCurrentPoint));
+}
+
+
+
+
+bool cd_library::cancel_step(fmi2Component c)
+{
+    return update_status_and_return_true_if_ok(fmi2CancelStep_(c));
+}
+
+bool cs_library::set_real_input_derivatives(
+        fmi2Component c,
+        const std::vector<fmi2ValueReference>& vr,
+        const std::vector<fmi2Integer>& order,
+        const std::vector<fmi2Real>& value)
+{
+    return fmi2SetRealInputDerivates_(c, vr.data(), vr.size(), order.data(), value.data());
+}
+
+bool cs_library::get_real_output_derivatives(
+        fmi2Component c,
+        const std::vector<fmi2ValueReference>& vr,
+        const std::vector<fmi2Integer>& order,
+        std::vector<fmi2Real>& value
+        )
+{
+    return update_status_and_return_true_if_ok(
+            fmi2GetRealOutputDerivates_(c, vr.data(), vr.size(), order.data(), value.data());
+}
+
+bool cs_library::get_status(
+        fmi2Component c,
+        const fmi2StatusKind s,
+        fmi2Status& value)
+{
+    return update_status_and_return_true_if_ok(
+            fmi2GetStatus_(c, s, &value);
+}
+
+bool cs_library::get_real_status(
+        fmi2Component c,
+        const fmi2StatusKind s,
+        fmi2Real& value)
+{
+    return update_status_and_return_true_if_ok(
+            fmi2GetRealStatus_(c, s, &value));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
