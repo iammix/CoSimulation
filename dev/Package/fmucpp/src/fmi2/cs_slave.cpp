d60f90810ac34f413f468492758d3a27b417b389
iammix <k.mixios@gmail.com>
2020-09-14 12:07:09 +0300

    Ch 14/09/2020 12:06 - CoSimulation slave functions

--- dev/Package/fmucpp/src/fmi2/cs_slave.cpp
+++ dev/Package/fmucpp/src/fmi2/cs_slave.cpp
@@ -17,6 +17,72 @@ fmicpp::status cs_slave::last_status() const
     return convert(library_->last_status());
 }
 
+bool cs_slave::step(const double stepSize)
+{
+    if (library_->step(c_, simulationTime_, stepSize, false)) {
+        simulationTIme_ += stepSize;
+        return true;
+    }
+    return false;
+}
+
+bool cs_slave::cancel_step()
+{
+    return library_->cancel_step(c_);
+}
+
+std::shared_ptr<const cs_model_description> cs_slave::get_model_description() const
+{
+    return fmu_instance_base::get_model_description();
+}
+
+
+bool cs_slave::setup_experiment(double start, double stop, double tolerance)
+{
+    return fmu_instance_base::setup_experiment(start, stop, tolerance);
+}
+
+bool cs_slave::enter_initialization_mode()
+{
+    return fmu_instance_base::enter_initialization_mode();
+}
+
+bool cs_slave::exit_initialization_mode()
+{
+    return fmu_instance_base::exit_initialization_mode();
+}
+
+bool cs_slave::reset()
+{
+    return fmu_instance_base::reset();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
