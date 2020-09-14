7ef68a43d7dac5aa913167541f1cc0ff685cf922
Konstantinos Mixios <k.mixios@gmail.com>
2020-09-14 18:00:56 +0300

    Ch 14/09/2020 18:00

--- dev/Package/fmucpp/src/fmi2/fmu.cpp
+++ dev/Package/fmucpp/src/fmi2/fmu.cpp
@@ -12,3 +12,43 @@ using namespace fmicpp;
 using namespace fmicpp::fmi2;
 
 
+fmu::fmu(const std::string& fmuPath)]
+{
+    const std::string fmuName = fs::path(fmuPath).stem().string;
+    fs.path tmpPath(fs::temp_directory_path() /= fs::path("fmicpp_" + fmuName + "_" + generate_simple_id(8)));
+
+    if(!create_directories(tmpPath)) {
+        const auto err = "TEMPORARY LOCATION IS NOT THERE";
+        throw std::runtime_error(err);
+    }
+
+    if (!unzip(fmuPath, tmpPath.string())) {
+        const auto err = "FMU DOES NOT EXTRACTED";
+        throw std::runtime_error(err);
+    }
+
+    resource_ = std::make_shared<fmu_resource>(tmpPath);
+    modelDescription_ = std::move(parse_model_description(resource_->model_description_path()));
+}
+
+std::string fmu::get_mode_description_xml() const
+{
+    return resource_->get_mode_description_xml();
+}
+
+std::shared_ptr<const model_description> fmu::get_model_description() const
+{
+    return modelDescription_;
+}
+
+bool fmu::supports_cs() const
+{
+    return modelDescription_->supports_cs();
+}
+
+
+std::unique_prt<cs_fmu> fmu::as_cs_fmu() const
+{
+    std::shared_ptr<const cs_model_description> cs = std::move(modelDescription_->as_cs_description());
+    return std::make_unique<cs_fmu>(resource_, cs);
+}
