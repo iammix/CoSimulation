94f67630705b08f5122618bfb6be3b5157501914
Konstantinos Mixios <k.mixios@gmail.com>
2020-09-15 18:53:13 +0300

    Ch 15/09/2020 18:52 - Definition of model types

---
+++ dev/Package/fmucpp/fmicpp/types.hpp
@@ -0,0 +1,18 @@
+#ifndef FMICPP_TYPES_HPP
+#define FMICPP_TYPES_HPP
+
+namespace fmicpp
+{
+    typedef void* fmicppComponent;
+    typedef void* fmicppFMUstate;
+    typedef unsigned int fmicppValueReference;
+    typedef double fmicppReal;
+    typedef int fmicppInteger;
+    typedef int fmicppBoolean;
+    typedef char fmicppChar;
+    typedef const fmicppChar* fmicppString;
+    typedef char fmicppByte;
+}
+
+
+#endif //FMICPP_TYPES_HPP
