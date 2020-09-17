a0d8c2727c6009d1c9796cf899004735f9be483e
Konstantinos Mixios <k.mixios@gmail.com>
2020-09-17 17:27:09 +0300

    Ch 17/09/2020 17:26 - create Dependent types

--- dev/Package/fmucpp/fmicpp/fmi2/cs_library.hpp
+++ dev/Package/fmucpp/fmicpp/fmi2/cs_library.hpp
@@ -14,7 +14,33 @@ namespace fmicpp::fmi2
         fmi2CancelStepTYPE* fmi2CancelStep_;
         fmi2GetStatusTYPE* fmi2GetStatus_;
         fmi2GetRealStatusTYPE* fmi2GetRealStatus_;
-        fmi2GetIntegerStatusTYPE* fmi2GetInteger_;
+        fmi2GetIntegerStatusTYPE* fmi2GetIntegerStatus_;
+        fmi2GetBooleanStatusTYPE* fmi2GetBooleanStatus_;
+        fmi2GetStringStatusTYPE* fmi2GetStringStatus_;
+    public:
+        cs_library(
+                const std::string& modelIdentifier,
+                const std::shared_ptr<fmu_resource>& resource);
+
+        bool step(fmi2Component c, fmi2Real currentCommunicationPoint,
+                  fmi2RealcommunicationStepSize, bool noSetFMUStatePriorToCurrentPoint);
+
+        bool cancel_step(fmi2Component c);
+        bool set_real_derivatives(fmi2Component c,
+                                  const std::vector<fmi2ValueReference>& vr,
+                                  const std::vector<fmi2Integer>& order,
+                                  const std::vector<fmi2Real>& value);
+
+        bool get_real_output_derivatives(fmi2Component c,
+                                         const std::vector<fmi2ValueReference>& vr,
+                                         const std::vector<fmi2Integer>& order,
+                                         std::vector<fmi2Real>& value);
+
+        bool get_status(fmi2Component c, fmi2StatusKind s, fmi2Status& value);
+        bool get_real_status(fmi2Component c, fmi2StatusKind s, fmi2Real& value);
+        bool get_integer_status(fmi2Component c, fmi2StatusKind s, fmi2Integer& value);
+        bool get_boolean_status(fmi2Component c, fmi2StatusKind s, fmi2Boolean& value);
+        bool get_string_status(fmi2Component c, fmi2StatusKind s, fmi2String& value);
     };
 }
 
---
+++ dev/Package/fmucpp/fmicpp/fmi2/cs_slave.hpp
@@ -0,0 +1,43 @@
+#ifndef FMI2_CS_SLAVE_HPP
+#define FMI2_CS_SLAVE_HPP
+
+#include <fmicpp/fmi2/cs_library.hpp>
+#include <fmicpp/fmi2/fmi2TypesPlatform.h>
+#include <fmicpp/fmi2/xml/model_description.hpp>
+#include <fmicpp/fmu_instance_base.hpp>
+#include <fmicpp/fmu_resource.hpp>
+#include <fmicpp/fmu_slave.hpp>
+
+#include <memory>
+
+namespace fmicpp::fmi2
+{
+    class cs_slave : public virtual fmu_slave<cs_model_description>,
+            public fmu_instance_base<cs_library, cs_model_description>
+    {
+    public:
+        cs_slave(fmi2Component c,
+                 const std::shared_ptr<fmu_resource>& resource,
+                 const std::shared_ptr<cs_library>& library,
+                 const std::shared_ptr<const cs_model_description>& modelDescription);
+        bool step(double stepSize) override;
+        bool cancel_step() override;
+
+        [[nodiscard]] std::shared_ptr<const cs_model_description> get_model_description() const override;
+        [[nodiscard]] status last_status() const override;
+
+        bool setup_experiment(double start = 0, double stop = 0, double tolerance = 0) override;
+        bool enter_initialization_mode() override;
+        bool exit_initialization_mode() override;
+
+        bool reset() override;
+        bool terminate() override;
+
+        bool read_integer(fmi2ValueReference vr, fmi2Integer& ref) override;
+        bool read_integer(const std::vector<fmi2ValueReference>& vr, std::vector<fmi2Integer>& ref) override;
+
+        bool read_real(fmi2ValueReference vr, fmi2Real& ref) override;
+    };
+}
+
+#endif //FMI2_CS_SLAVE_HPP
