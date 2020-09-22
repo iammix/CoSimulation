ba5eac94c7cabd34893c08d9c74a4ac2462de79e
Konstantinos Mixios <k.mixios@gmail.com>
2020-09-22 15:30:56 +0300

    Ch 22/09/2020 15:29 - fmi2functions

---
+++ dev/Package/fmucpp/fmicpp/fmi2/fmi2.hpp
@@ -0,0 +1,16 @@
+#ifndef FMICPP_FMI2_HPP
+#define FMICPP_FMI2_HPP
+
+#include <fmicpp/fmi2/cs_fmu.hpp>
+#include <fmicpp/fmi2/cs_slave.hpp>
+#include <fmicpp/fmi2/fmu.hpp>
+#include <fmicpp/fmi2/me_fmu.hpp>
+#include <fmicpp/fmi2/xml/enums.hpp>
+#include <fmicpp/fmi2/xml/model_description.hpp>
+#include <fmicpp/fmi2/xml/model_description_parser.hpp>
+#include <fmicpp/fmi2/xml/typed_scalar_variable.hpp>
+
+
+
+
+#endif //FMICPP_FMI2_HPP
