b11a92933cf5fd390538ed0133628a082603864d
Konstantinos Mixios <k.mixios@gmail.com>
2020-09-22 13:11:59 +0300

    Ch 22/09/2020 13:11

---
+++ dev/Package/fmucpp/fmicpp/fmi2/fmi2_library.hpp
@@ -0,0 +1,24 @@
+#ifndef FMI2_FMI2_LIBRARY_HPP
+#define FMI2_FMI2_LIBRARY_HPP
+
+#include <fmicpp/dll_handle.hpp>
+#include <fmicpp/fmi2/fmi2FunctionTypes.h>
+#include <fmicpp/fmu_resource.hpp>
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+namespace fmicpp::fmi2
+{
+    class fmi2_library
+    {
+    private:
+        std::shared_ptr<fmu_resource>
+    };
+}
+
+
+
+#endif //FMI2_FMI2_LIBRARY_HPP
